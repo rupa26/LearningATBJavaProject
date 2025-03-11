bceb4c83b5ccf76e9101b781696a8476dcf92c65
rupa26 <ruparanijps202@gmail.com>
2025-03-12 01:11:55 +0530

    Practice Multiplication of 9

---
+++ src/ex_01_Java_Basic_part2/Lab028_Table_Of_9.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic_part2;
+
+public class Lab028_Table_Of_9 {
+    public static void main(String[] args) {
+        int num = 9;
+        System.out.println("Multiplication of "+num+":");
+        System.out.printf("%d*1=%d%n" ,num,(num));
+        System.out.printf("%d*2=%d%n",num,(num*2));
+        System.out.printf("%d*3=%d%n",num,(num*3));
+        System.out.printf("%d*4=%d%n",num,(num*4));
+        System.out.printf("%d*5=%d%n",num,(num*5));
+        System.out.printf("%d*6=%d%n",num,(num*6));
+        System.out.printf("%d*7=%d%n",num,(num*7));
+        System.out.printf("%d*8=%d%n",num,(num*8));
+        System.out.printf("%d*9=%d%n",num,(num*9));
+        System.out.printf("%d*10=%d%n",num,(num*10));
+    }
+}
+
