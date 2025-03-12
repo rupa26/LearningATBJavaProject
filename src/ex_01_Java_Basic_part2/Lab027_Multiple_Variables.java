5513f3b6f4c5474f706ef88ac3a50ad43e751508
rupa26 <ruparanijps202@gmail.com>
2025-03-13 01:33:05 +0530

    12 March Practice

--- src/ex_01_Java_Basic_part2/Lab027_Multiple_Variables.java
+++ src/ex_01_Java_Basic_part2/Lab027_Multiple_Variables.java
@@ -1,7 +1,4 @@
 package ex_01_Java_Basic_part2;
-
-import java.sql.SQLOutput;
-
 public class Lab027_Multiple_Variables {
     public static void main(String[] args) {
         int a = 10, b = 23, c = 90;
