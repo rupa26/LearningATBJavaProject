f76d7d2da813afcace0dcbb8efb52df689cc8aa1
rupa26 <ruparanijps202@gmail.com>
2025-03-19 09:24:43 +0530

    14 March Task

--- src/Lab_Task/Task002_In_de_Operator.java
+++ src/Lab_Task/Task002_In_de_Operator.java
@@ -1,6 +1,6 @@
 package Lab_Task;
 
-public class Task002_In_de_Opertor {
+public class Task002_In_de_Operator {
     public static void main(String[] args) {
         int a= 10;
         System.out.println(++a + a++ + a++);
