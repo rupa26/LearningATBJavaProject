4ba3eadf490f2bcf7f8cb95cebb81f3594d6722d
rupa26 <ruparanijps202@gmail.com>
2025-03-19 09:23:23 +0530

    14 March Task

--- src/Lab_Task/Task002_In_de_Opertor.java
+++ src/Lab_Task/Task002_In_de_Opertor.java
@@ -5,5 +5,11 @@ public class Task002_In_de_Opertor {
         int a= 10;
         System.out.println(++a + a++ + a++);
         System.out.println(a);
+
+        // ++a A-> 11  , a=11
+        // a++ B-> 11  , a=12
+        // a++ C-> 12 , a=13
+        // A+B+C = 34
+
     }
 }
--- src/Lab_Task/Task003_In_De_op_2.java
+++ src/Lab_Task/Task003_In_De_op_2.java
@@ -4,6 +4,11 @@ public class Task003_In_De_op_2 {
     public static void main(String[] args) {
         int a =20;
         System.out.println(--a + a++ + a--);
-        System.out.println(a );
+        System.out.println(a);
+        // --a -> 19  , a=19
+        // a++ -> 19  , a=20
+        // a-- -> 20 , a=19
+        // A+B+C = 58 , a= 19
+
     }
 }
